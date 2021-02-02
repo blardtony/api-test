bbcf267770a12698af23f58b040bc7626dedb44f
julien szal <jszal@sdis55.fr>
2021-02-02 09:42:23 +0100

    POST

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\Normalizer\NormalizableInterface;
 use Symfony\Component\Serializer\SerializerInterface;
+
 
 /**
  * Class ArticleController
@@ -24,5 +26,18 @@ class ArticleController extends AbstractController
     public function getarticle(ArticleRepository $articleRepository): JsonResponse
     {
         return $this->json($articleRepository->findAll(), 200, [], ['groups' => 'get']);
+    }
+
+
+    /**
+     * @Route("/article", name="postarticle", methods={"POST"})
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function postarticle(Request $request,SerializerInterface $serializer): JsonResponse
+    {
+       $json = $request->getContent();
+       $article = $serializer->deserialize($json,Article::class,"json");
+       dd($article);
     }
 }
